d028ca8c81afe077f67f1948443e49ad68800021
Karol Lassak <karol.lassak@allegrogroup.com>
2016-02-22 19:42:06 +0100

    Dont force puppet

--- examples/5_jenkins_dsl/manifests/prepare_environment.pp
+++ examples/5_jenkins_dsl/manifests/prepare_environment.pp
@@ -5,7 +5,7 @@ exec { 'build_jenkins_module':
 }
 
 exec { 'install_jenkins_module':
-  command => 'puppet module install --force pkg/cmw-jenkins_dsl-0.1.0.tar.gz',
+  command => 'puppet module install pkg/cmw-jenkins_dsl-0.1.0.tar.gz',
   cwd     => '/vagrant/cmw-jenkins_dsl',
   path    => ['/bin', '/usr/bin'],
   require => Exec['build_jenkins_module'],
