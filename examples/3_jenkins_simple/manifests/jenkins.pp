d0636477acd40ac0894d6a0588113d1fff4cc23e
Karol Lassak <karol.lassak@allegrogroup.com>
2016-02-22 12:40:08 +0100

    Working dsl example

--- examples/3_jenkins_simple/manifests/jenkins.pp
+++ examples/3_jenkins_simple/manifests/jenkins.pp
@@ -1,31 +1,65 @@
+package {'git': }
+package {'maven': }
+
+
 class { 'jenkins':
-  executors => 0,
 }
 
+jenkins::plugin { 'icon-shim': }
+jenkins::plugin { 'scm-api': }
+jenkins::plugin { 'ssh-credentials': }
+jenkins::plugin { 'git-client': }
 jenkins::plugin { 'git': }
 
-jenkins::user { 'ingwar':
-  email    => 'ingwar@example.com',
-  password => 'notMine',
-}
 
-jenkins::job { 'test-build' :
+jenkins::plugin { 'job-dsl': }
+
+jenkins::job { 'dsl-build' :
     config => '<?xml version=\'1.0\' encoding=\'UTF-8\'?>
 <project>
   <actions/>
   <description></description>
   <keepDependencies>false</keepDependencies>
-  <properties></properties>
-  <scm class="hudson.scm.NullSCM"/>
+  <properties/>
+  <scm class=\'hudson.scm.NullSCM\'/>
   <canRoam>true</canRoam>
   <disabled>false</disabled>
   <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
   <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
   <triggers/>
   <concurrentBuild>false</concurrentBuild>
-  <builders/>
+  <builders>
+    <javaposse.jobdsl.plugin.ExecuteDslScripts plugin=\'job-dsl@1.43\'>
+      <scriptText>def project = &apos;quidryan/aws-sdk-test&apos;
+def branchApi = new URL(&quot;https://api.github.com/repos/${project}/branches&quot;)
+def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+branches.each {
+    def branchName = it.name
+    def jobName = &quot;${project}-${branchName}&quot;.replaceAll(&apos;/&apos;,&apos;-&apos;)
+    job(jobName) {
+        scm {
+            git(&quot;git://github.com/${project}.git&quot;, branchName)
+        }
+        steps {
+            maven(&quot;test -Dproject.name=${project}/${branchName}&quot;)
+        }
+    }
+}</scriptText>
+      <usingScriptText>true</usingScriptText>
+      <ignoreExisting>false</ignoreExisting>
+      <removedJobAction>IGNORE</removedJobAction>
+      <removedViewAction>IGNORE</removedViewAction>
+      <lookupStrategy>JENKINS_ROOT</lookupStrategy>
+      <additionalClasspath></additionalClasspath>
+    </javaposse.jobdsl.plugin.ExecuteDslScripts>
+  </builders>
   <publishers/>
   <buildWrappers/>
 </project>';
 }
 
+#jenkins::user { 'ingwar':
+#  email    => 'ingwar@example.com',
+#  password => 'notMine',
+#}
+#
--- examples/3_jenkins_simple/manifests/prepare_environment.pp
+++ examples/3_jenkins_simple/manifests/prepare_environment.pp
@@ -1,10 +1,9 @@
-package { 'htop':
-  ensure  => 'installed',
-}
-
-
-exec {'install_jenkins_module':
+exec { 'install_jenkins_module':
   command => 'puppet module install rtyler-jenkins',
   unless  => 'puppet module list | grep rtyler-jenkins',
+  path    => ['/bin', '/usr/bin'],
+}
+
+exec { 'apt-get update':
   path    => ['/bin', '/usr/bin'],
 }
